355b769b0de79356f6b572d39f42be47271f6517
Garrit Schaap <contact@garritschaap.com>
2019-06-13 15:07:03 +0200

    Initial Commit

---
+++ FaceTracking/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FaceTracking
+//
+//  Created by Garrit on 13.06.19.
+//  Copyright © 2019 Garrit GmbH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
