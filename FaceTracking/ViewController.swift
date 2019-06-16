2d6be492e915e3d546cc0615d78b9b2ea348183a
Garrit Schaap <contact@garritschaap.com>
2019-06-16 12:55:20 +0200

    Added FaceTracking to App

--- FaceTracking/ViewController.swift
+++ FaceTracking/ViewController.swift
@@ -7,14 +7,54 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let configuration = ARFaceTrackingConfiguration()
+        (view as? ARSCNView)?.session.run(configuration)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        guard ARFaceTrackingConfiguration.isSupported else {
+            fatalError("Face Tracking not supported!")
+        }
+        (view as? ARSCNView)?.delegate = self
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        (view as? ARSCNView)?.session.pause()
     }
 
+}
 
+extension ViewController: ARSCNViewDelegate {
+    
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        guard let device = MTLCreateSystemDefaultDevice() else {
+            return nil
+        }
+        let faceGeometry = ARSCNFaceGeometry(device: device)
+        let node = SCNNode(geometry: faceGeometry)
+        node.geometry?.firstMaterial?.fillMode = .lines
+        return node
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let faceAnchor = anchor as? ARFaceAnchor,
+            let faceGeometry = node.geometry as? ARSCNFaceGeometry else {
+                return
+        }
+        
+        faceGeometry.update(from: faceAnchor.geometry)
+    }
+    
 }
 
